5bcf3765457dd2a12c85156ec95acd199e7f767e
Ryan Welton <rwelton24@gmail.com>
2012-08-05 15:54:10 -0400

    Add settings for fast charge power widget
    Change-Id: If8db5e2a41de9692c466dbf08ea9cfbb0093d92c

--- src/com/android/settings/cyanogenmod/PowerWidgetUtil.java
+++ src/com/android/settings/cyanogenmod/PowerWidgetUtil.java
@@ -55,6 +55,7 @@ public class PowerWidgetUtil {
     public static final String BUTTON_MEDIA_NEXT = "toggleMediaNext";
     public static final String BUTTON_LTE = "toggleLte";
     public static final String BUTTON_WIMAX = "toggleWimax";
+    public static final String BUTTON_OVERCHARGE = "toggleOverCharge";
 
     public static final HashMap<String, ButtonInfo> BUTTONS = new HashMap<String, ButtonInfo>();
     static {
@@ -120,6 +121,10 @@ public class PowerWidgetUtil {
         BUTTONS.put(BUTTON_WIMAX, new PowerWidgetUtil.ButtonInfo(
                 BUTTON_WIMAX, R.string.title_toggle_wimax,
                 "com.android.systemui:drawable/stat_wimax_on"));
+
+        BUTTONS.put(BUTTON_OVERCHARGE, new PowerWidgetUtil.ButtonInfo(
+                BUTTON_OVERCHARGE, R.string.title_toggle_fast_charge,
+                "com.android.systemui:drawable/stat_over_charge_off"));
     }
 
     private static final String BUTTON_DELIMITER = "|";
